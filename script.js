d6a038eedccb66d7b70c7351db81c27ad3fb9f76
tobias-wilfert <s0181439@ad.ua.ac.be>
2022-02-08 22:46:41 +0100

    Added output + js test

--- script.js
+++ script.js
@@ -1,2 +1,18 @@
 // Make a function that gets fired when the submit button is pressed
 // Need to calculate the values and output them to the dom in a nice fashion
+$('#submit').click(function (event) {
+    // Avoids clearing the fields
+    event.preventDefault();
+
+    // Show the hidden output
+    $("#output").toggleClass("visually-hidden");
+
+    // How to grab the value
+    alert(Number($('#CH1-5').val()));
+
+    // TODO: Do the calculations
+
+    // How to assign  the value
+    $('#CH2-5').val('13');
+    $('#O-CH2-5').val('13');
+});
